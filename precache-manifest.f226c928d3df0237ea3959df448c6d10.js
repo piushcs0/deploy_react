e6e7fcfb35902df39b89b05c7175c97f1d8cf98b
piush <piush3898@gmail.com>
2020-05-05 13:16:29 +0530

    Updates

---
+++ precache-manifest.f226c928d3df0237ea3959df448c6d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27766170085a43b6b06283f4cb308c27",
+    "url": "/deploy_react/index.html"
+  },
+  {
+    "revision": "d4484d7595b12a686a07",
+    "url": "/deploy_react/static/css/main.b02d84db.chunk.css"
+  },
+  {
+    "revision": "b58d391947dcd2914831",
+    "url": "/deploy_react/static/js/2.037c14d6.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/deploy_react/static/js/2.037c14d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4484d7595b12a686a07",
+    "url": "/deploy_react/static/js/main.3f742b1d.chunk.js"
+  },
+  {
+    "revision": "e8d535e7b92376addb1d",
+    "url": "/deploy_react/static/js/runtime-main.b7314259.js"
+  }
+]);
